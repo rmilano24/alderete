f66a4c284e5186498a22a9fc4724e864ea48971a
rmilano24 <ryan@nmbl.digital>
2025-07-03 21:22:22 -0700

    start building out inner pages

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,58 @@
+<script lang="ts">
+	import home_hero from '$lib/images/bg_home-hero.jpg';
+</script>
+
 <svelte:head>
 	<title>NMBL | Branding and Design Studio in Santa Cruz, California</title>
 	<meta name="description" content="NMBL | UX Design, Branding, Web Design & Development Studio in Santa Cruz | San Francisco Bay Area" />
 </svelte:head>
 
-<h1 class="text-5xl">Hello world</h1>
+<section id="hero">
+	<div class="grid grid-cols-2 gap-4 ">
+		<div class="p-24 mt-40">
+			<h5 class="text-white uppercase text font-matter tracking-widest after:bg-white after:content-[''] after:inline-block after:h-0.5 after:ml-6 after:w-8 after:relative after:bottom-[5px]">Call us or fill out the form below</h5>
+			<h1 class="text-5xl text-teal-100 font-reckless mt-8">Request appointment</h1>
+			<p class="text-teal-100 mt-8 text-lg">At our office you'll find a friendly and professional staff, each dedicated to providing your family with quality care and personal attention. They are trained professionals with many years of experience, and a desire to share their knowledge so that you may achieve a healthy, attractive and comfortable mouth.</p> 
+			
+
+		</div>
+		<div class="h-[900px] bg-center bg-cover bg-no-repeat rounded-bl-4xl" style="background-image: url('{home_hero}')"></div>
+	  </div>
+</section>
+
+<div id="cta-boxes">
+	<div class="grid grid-cols-3 gap-8 container mx-auto mb-30">
+
+		<div class="mt-40">
+			<div class="glass rounded-2xl border border-white/20 shadow-[0_4px_100px_0px_rgba(0,0,0,0.6)] flex flex-col justify-between p-8 w-full">
+				<div>
+					<svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M1 4.55556C1 3.61256 1.3746 2.70819 2.0414 2.0414C2.70819 1.3746 3.61256 1 4.55556 1H10.3867C10.7596 1.00028 11.1231 1.11786 11.4255 1.33609C11.728 1.55432 11.9541 1.86214 12.072 2.216L14.7351 10.2036C14.87 10.6095 14.8541 11.0505 14.6902 11.4456C14.5262 11.8407 14.2253 12.1635 13.8427 12.3547L9.83022 14.3636C11.797 18.7165 15.2835 22.203 19.6364 24.1698L21.6453 20.1573C21.8365 19.7747 22.1593 19.4738 22.5544 19.3098C22.9495 19.1459 23.3905 19.13 23.7964 19.2649L31.784 21.928C32.1381 22.046 32.4462 22.2724 32.6644 22.5752C32.8827 22.878 33.0001 23.2418 33 23.6151V29.4444C33 30.3874 32.6254 31.2918 31.9586 31.9586C31.2918 32.6254 30.3874 33 29.4444 33H27.6667C12.9396 33 1 21.0604 1 6.33333V4.55556Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+					</svg>
+				</div>
+			<div class="text-3xl"><a target="_blank" href="tel:18314323231">831.432.3231</a></div>
+		</div>
+		</div>
+		<div class="mt-40">
+			<div class="glass rounded-2xl border border-white/20 shadow-[0_4px_100px_0px_rgba(0,0,0,0.6)] flex flex-col justify-between p-8">
+				<div>
+					<svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M1 4.55556C1 3.61256 1.3746 2.70819 2.0414 2.0414C2.70819 1.3746 3.61256 1 4.55556 1H10.3867C10.7596 1.00028 11.1231 1.11786 11.4255 1.33609C11.728 1.55432 11.9541 1.86214 12.072 2.216L14.7351 10.2036C14.87 10.6095 14.8541 11.0505 14.6902 11.4456C14.5262 11.8407 14.2253 12.1635 13.8427 12.3547L9.83022 14.3636C11.797 18.7165 15.2835 22.203 19.6364 24.1698L21.6453 20.1573C21.8365 19.7747 22.1593 19.4738 22.5544 19.3098C22.9495 19.1459 23.3905 19.13 23.7964 19.2649L31.784 21.928C32.1381 22.046 32.4462 22.2724 32.6644 22.5752C32.8827 22.878 33.0001 23.2418 33 23.6151V29.4444C33 30.3874 32.6254 31.2918 31.9586 31.9586C31.2918 32.6254 30.3874 33 29.4444 33H27.6667C12.9396 33 1 21.0604 1 6.33333V4.55556Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+					</svg>
+				</div>
+			<div class="text-3xl"><a target="_blank" href="tel:18314323231">831.432.3231</a></div>
+		</div>
+		</div>
+		<div class="mt-40">
+			<div class="glass rounded-2xl border border-white/20 shadow-[0_4px_100px_0px_rgba(0,0,0,0.6)] flex flex-col justify-between p-8">
+				<div>
+					<svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M1 4.55556C1 3.61256 1.3746 2.70819 2.0414 2.0414C2.70819 1.3746 3.61256 1 4.55556 1H10.3867C10.7596 1.00028 11.1231 1.11786 11.4255 1.33609C11.728 1.55432 11.9541 1.86214 12.072 2.216L14.7351 10.2036C14.87 10.6095 14.8541 11.0505 14.6902 11.4456C14.5262 11.8407 14.2253 12.1635 13.8427 12.3547L9.83022 14.3636C11.797 18.7165 15.2835 22.203 19.6364 24.1698L21.6453 20.1573C21.8365 19.7747 22.1593 19.4738 22.5544 19.3098C22.9495 19.1459 23.3905 19.13 23.7964 19.2649L31.784 21.928C32.1381 22.046 32.4462 22.2724 32.6644 22.5752C32.8827 22.878 33.0001 23.2418 33 23.6151V29.4444C33 30.3874 32.6254 31.2918 31.9586 31.9586C31.2918 32.6254 30.3874 33 29.4444 33H27.6667C12.9396 33 1 21.0604 1 6.33333V4.55556Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+					</svg>
+				</div>
+			<div class="text-3xl"><a target="_blank" href="tel:18314323231">831.432.3231</a></div>
+		</div>
+		</div>
+
+	  </div>
+</div>
--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,6 +1,77 @@
+<script lang="ts">
+	import home_hero from '$lib/images/bg_home-hero.jpg';
+	import cta from '$lib/images/cta.jpg';
+</script>
+
 <svelte:head>
 	<title>NMBL | Branding and Design Studio in Santa Cruz, California</title>
 	<meta name="description" content="NMBL | UX Design, Branding, Web Design & Development Studio in Santa Cruz | San Francisco Bay Area" />
 </svelte:head>
 
-<h1 class="text-5xl">Hello world</h1>
+<section id="hero">
+	<div class="grid grid-cols-2 gap-4 ">
+		<div class="p-24 mt-40">
+			<h5 class="text-white uppercase text font-matter tracking-widest after:bg-white after:content-[''] after:inline-block after:h-0.5 after:ml-6 after:w-8 after:relative after:bottom-[5px]">What we’re good at</h5>
+			<h1 class="text-5xl text-teal-100 font-reckless mt-8">Our services</h1>
+			<p class="text-teal-100 mt-8 text-lg">You want your smile to be the best it can be, and we are here to help. Dr Alderete and his caring, friendly team of dental professionals are committed to providing a comfortable dental experience. Our goal is to provide dental care for you and your family in a compassionate, caring and professional environment.</p> 
+			
+
+		</div>
+		<div class="h-[900px] bg-center bg-cover bg-no-repeat rounded-bl-4xl" style="background-image: url('{home_hero}')"></div>
+	  </div>
+</section>
+
+
+
+<section id="expertise">
+
+	<div class="container m-auto">
+
+		<h5 class="text-white uppercase text font-matter tracking-widest mb-16 after:bg-white after:content-[''] after:inline-block after:h-0.5 after:ml-6 after:w-8 after:relative after:bottom-[5px]">Our Areas of Expertise</h5>
+
+		<div class="grid grid-cols-2 gap-16">
+
+			<div class="border-b border-teal-800 pb-16">
+				<span class="block text-teal-100">01</span>
+				<h2 class="text-3xl font-reckless mt-1 text-teal-100">Family & General Dentistry</h2>
+			</div>
+			<div class="border-b border-teal-800 pb-16">
+				<span class="block text-teal-100">02</span>
+				<h2 class="text-3xl font-reckless mt-1 text-teal-100">Restorative Dentistry</h2>
+			</div>
+			<div class="border-b border-teal-800 pb-16">
+				<span class="block text-teal-100">03</span>
+				<h2 class="text-3xl font-reckless mt-1 text-teal-100">Cosmetic Dentistry</h2>
+			</div>
+			<div class="border-b border-teal-800 pb-16">
+				<span class="block text-teal-100">04</span>
+				<h2 class="text-3xl font-reckless mt-1 text-teal-100">Periodontal Services</h2>
+			</div>
+			<div class="border-b border-teal-800 pb-16">
+				<span class="block text-teal-100">05</span>
+				<h2 class="text-3xl font-reckless mt-1 text-teal-100">Surgical & Endodontic Services</h2>
+			</div>
+			<div class="border-b border-teal-800 pb-16">
+				<span class="block text-teal-100">06</span>
+				<h2 class="text-3xl font-reckless mt-1 text-teal-100">Digital x-rays and Panorex x-rays</h2>
+			</div>
+			<div class="border-b border-teal-800 pb-16">
+				<span class="block text-teal-100">07</span>
+				<h2 class="text-3xl font-reckless mt-1 text-teal-100">Intra-oral Camera</h2>
+			</div>
+
+
+		  </div>
+	</div>
+
+	
+</section>
+
+
+<section id="cta" class="h-[724px] bg-cover flex items-center justify-center" style="background-image: url('{cta}')">
+	<div class="glass rounded-2xl border border-white/20 flex w-4xl flex-col items-center text-center py-16 px-32">
+		<div><h1 class="text-6xl">We're looking forward to <span class="italic">meeting you.</span></h1></div>
+		<div class="mt-6"><p class="text">Feel free to drop in or give us a call at 831.432.3231.</p></div>
+		<div class="mt-8"><a class="glass rounded-2xl border border-white/20 p-4 block" href="/contact">Request appointment</a></div>
+	  </div>
+</section>
