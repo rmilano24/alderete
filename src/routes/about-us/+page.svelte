4b387e2ac66bbce0c136a26bf8673d91bfcb4556
rmilano24 <ryan@nmbl.digital>
2025-07-10 14:32:47 -0700

    remove !

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -57,7 +57,7 @@
 
   <div id="jim-alderete" class="modal-window glass">
 	<div>
-	  <a href="#!" title="Close" class="modal-close glass rounded-2xl border border-white/20 p-6 m-8">
+	  <a href="#" title="Close" class="modal-close glass rounded-2xl border border-white/20 p-6 m-8">
 		<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path d="M1 13L13 1M1 1L13 13" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 		</svg>			
@@ -130,7 +130,7 @@
 
   <div id="ryan-reyes" class="modal-window glass">
 	<div class="glass">
-	  <a href="#!" title="Close" class="modal-close glass rounded-2xl border border-white/20 p-6 m-8">
+	  <a href="#" title="Close" class="modal-close glass rounded-2xl border border-white/20 p-6 m-8">
 		<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path d="M1 13L13 1M1 1L13 13" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 		</svg>			
