2a0571062a24bd26968671bcc9a1005501c116e0
rmilano24 <ryan@nmbl.digital>
2025-07-21 21:25:00 -0700

    update cta

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -24,7 +24,7 @@
 
 	import jim from '$lib/images/jim-alderete.jpg';
 	import ryan from '$lib/images/about/ryan-reyes.jpg';
-	import cta from '$lib/images/cta.jpg';
+	import cta from '$lib/images/cta-explore.jpg';
 	import jimlg from '$lib/images/about/jim-alderete-lg.jpg';
 	import belermine from '$lib/images/about/seals/seal-belermine.png';
 	import ucsb from '$lib/images/about/seals/seal-ucsb.png';
@@ -333,6 +333,13 @@ $effect(() => {
 </section>
 
 
-<BottomCta />
+<section id="cta" class="relative mt-30 max-md:mt-13 h-[724px] max-lg:h-[500px] bg-cover flex items-center justify-center" style="background-image: url('{cta}')">
+	<div class="absolute inset-0 bg-black/20 pointer-events-none z-0"></div>
+	<div class="relative glass rounded-2xl border max-w-[830px] border-white/20 flex w-4xl flex-col items-center text-center py-16 px-24 mx-5 max-lg:px-12 max-sm:px-8 z-10">
+		<div><h1 class="text-6xl max-lg:text-4xl font-reckless tracking-tight">Our modern equipment ensures <span class="italic">optimal results.</span></h1></div>
+		<div class="mt-6"><p class="text-lg">We're always staying up to date with the latest in technology.</p></div>
+		<div class="mt-8"><a class="glass rounded-2xl border border-white/20 p-4 block font-bold" href="/gallery">Explore our office</a></div>
+	  </div>
+</section>
 
 
--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Service from '$lib/components/service.svelte';
 	import home_hero from '$lib/images/bg_home-hero.jpg';
-	import cta from '$lib/images/cta.jpg';
+	import cta from '$lib/images/cta-explore.jpg';
 	import BottomCta from '$lib/components/bottom-cta.svelte';
 	const restorativeDentistry = ["Tooth Colored Composite Fillings", "Amalgam Fillings", "Crowns and Bridges", "Removable Bridges and Dentures", "TMD Treatment and Appliances", "Implant Restorations"];
 	const CosmeticDentistry = ["White Composite Bonding", "All-Porcelain Crowns and Bridges", "Professional Whitening", "Veneers"];
@@ -46,4 +46,11 @@
 	
 </section>
 
-<BottomCta />
+<section id="cta" class="relative mt-30 max-md:mt-13 h-[724px] max-lg:h-[500px] bg-cover flex items-center justify-center" style="background-image: url('{cta}')">
+	<div class="absolute inset-0 bg-black/20 pointer-events-none z-0"></div>
+	<div class="relative glass rounded-2xl border max-w-[830px] border-white/20 flex w-4xl flex-col items-center text-center py-16 px-24 mx-5 max-lg:px-12 max-sm:px-8 z-10">
+		<div><h1 class="text-6xl max-lg:text-4xl font-reckless tracking-tight">Our modern equipment ensures <span class="italic">optimal results.</span></h1></div>
+		<div class="mt-6"><p class="text-lg">We're always staying up to date with the latest in technology.</p></div>
+		<div class="mt-8"><a class="glass rounded-2xl border border-white/20 p-4 block font-bold" href="/gallery">Explore our office</a></div>
+	  </div>
+</section>
