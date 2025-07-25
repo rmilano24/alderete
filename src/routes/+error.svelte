871844c324a3045710ed67f07b3368ce9f096113
rmilano24 <ryan@nmbl.digital>
2025-07-25 12:35:42 -0700

    add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>Page Not Found | Jim Alderete DDS</title>
+	<meta name="description" content="Jim Alderete DDS | Dentist in Santa Cruz, Ca" />
+</svelte:head>
+
+<section id="forms" class="container px-10 max-md:px-5 max-xl:max-w-full py-50 max-md:py-32 mx-auto">
+
+	<div class="grid grid-cols-3 max-lg:grid-cols-1 gap-38 max-xl:gap-24 max-lg:gap-8">
+		<div id="left-col">
+			<h1 class="text-7xl text-teal-100 font-reckless mt-8">404</h1>
+			<p class="text-teal-100 mt-4 text-lg leading-[32px]">Page not found</p>
+		</div>
+
+	</div>
+
+</section>
+
